2880d0f1fda46f082ded8cb539a83c90014f87d6
e-3va <142951737+e-3va@users.noreply.github.com>
2024-04-18 16:10:55 -0700

    Update RandomStringChooser.java

--- src/main/java/RandomStringChooser.java
+++ src/main/java/RandomStringChooser.java
@@ -6,17 +6,20 @@ public class RandomStringChooser
   //You will get a very confusing error message until you have working code in part b as well
 
   private ArrayList<String> words;
+  
   public RandomStringChooser(String[] arr) {
     words = new ArrayList<String>();
     for(String i : arr) {
       words.add(arr.get(i));
     }
   }
+  
   public String getNext() {
     if (words.size() > 0) {
       String temp = words.get((int)(Math.random() * words.length());
       words.remove(temp);
       return temp;
     }
-    else return "NONE";
+    else return "NONE"; 
+  }
 }

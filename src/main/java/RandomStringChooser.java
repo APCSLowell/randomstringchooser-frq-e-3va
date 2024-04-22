e975e63c4837cdb680fc1faaaf77a221f8d6c80e
e-3va <142951737+e-3va@users.noreply.github.com>
2024-04-22 15:47:33 -0700

    Update RandomStringChooser.java

--- src/main/java/RandomStringChooser.java
+++ src/main/java/RandomStringChooser.java
@@ -5,27 +5,22 @@ public class RandomStringChooser
   //Heads up! 
   //You will get a very confusing error message until you have working code in part b as well
 
-  private String[] arr;
-  private int valuesRemaining;
-  
-  public RandomStringChooser(String[] arr) {
-    arr = new String[arr.length];
-    for(int i = 0; i < arr.length; i++) {
-      arr[i] = arr[i];
+    private String[] arr;
+    private int valuesRemaining;
+    public RandomStringChooser(String[] vals) {
+        arr = new String[vals.length];
+        for(int i = 0; i < values.length; i++) {
+            arr[i] = vals[i];
+        }
+        valuesRemaining = arr.length;
     }
-    valuesRemaining = arr.length;
-  }
-  
-  public String getNext() {
-    if (valuesRemaining == 0) {
-      return "NONE";
+    public String getNext() {
+        if(valuesRemaining == 0)
+            return "NONE";
+        int index = (int) (Math.random() * valuesRemaining);
+        String selected = arr[index];
+        arr[index] = arr[valuesRemaining - 1];
+        valuesRemaining--;
+        return selected;
     }
-    else {
-      int index = (int)(Math.random() * valuesRemaining);
-      String selected = arr[index];
-      arr[index] = arr[valuesRemaining-1];
-      valuesRemaining--;
-      return selected;
-    }
-  }
 }

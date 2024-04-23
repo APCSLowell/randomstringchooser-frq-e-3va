96180e9a76ed7214362cc187bf21f007aa77b1d0
e-3va <142951737+e-3va@users.noreply.github.com>
2024-04-22 17:07:18 -0700

    Update RandomStringChooser.java

--- src/main/java/RandomStringChooser.java
+++ src/main/java/RandomStringChooser.java
@@ -5,22 +5,21 @@ public class RandomStringChooser
   //Heads up! 
   //You will get a very confusing error message until you have working code in part b as well
 
-    private String[] arr;
-    private int valuesRemaining;
-    public RandomStringChooser(String[] vals) {
-        arr = new String[vals.length];
-        for(int i = 0; i < arr.length; i++) {
-            arr[i] = vals[i];
-        }
-        valuesRemaining = arr.length;
+  private ArrayList<String> words;
+  
+  public RandomStringChooser(String[] arr) {
+    words = new ArrayList<String>();
+    for(String i : arr) {
+      words.add(i);
     }
-    public String getNext() {
-        if(valuesRemaining == 0)
-            return "NONE";
-        int index = (int) (Math.random() * valuesRemaining);
-        String selected = arr[index];
-        arr[index] = arr[valuesRemaining - 1];
-        valuesRemaining--;
-        return selected;
+  }
+  
+  public String getNext() {
+    if (words.size() > 0) {
+      String temp = words.get((int)(Math.random() * words.size()));
+      words.remove(temp);
+      return temp;
     }
+    else return "NONE"; 
+  }
 }
